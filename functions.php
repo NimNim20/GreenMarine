39dbde4bb0af95b58cfe13b1d0169d907afbf6a0
NimNim20 <benvio01@easv365.dk>
2024-11-19 11:10:58 +0100

    Styling test

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
-function gmd_enqueue_styles() {
-  wp_enqueue_style("gmd-style", get_template_directory_uri() . "/style.css", array(), "1.0", "all");
+function gmd_load_resources() {
+  wp_enqueue_style("style", get_template_directory_uri() . "/style.css");
 }
 
-add_action("wp_enqueue_scripts", "gmd_enqueue_styles");
+add_action("wp_enqueue_scripts", "gmd_load_resources");
