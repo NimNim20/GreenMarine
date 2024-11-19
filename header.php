1c5d76873033b3e7a810f5a78f139d150f0657f0
Simon Nyblom <simnyb01@easv365.dk>
2024-11-19 12:54:48 +0100

    removal of pictures

--- header.php
+++ header.php
@@ -12,10 +12,6 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
-                    <img src="images/phone.png" alt="phone">
-                    <img src="images/email.png" alt="email">
-                </div>
-                <div class="col-lg-6">
                     <h1>Kontakt</h1>
                 </div>
             </div>
