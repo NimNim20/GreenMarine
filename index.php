6b062b1c381f2bc787c366e1ab8113eac06b94ec
NimNim20 <benvio01@easv365.dk>
2024-11-19 11:25:32 +0100

    Hello world!

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
-    
+<?php get_header(); ?>
 
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
 
+  <section class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Hello world!</h1>
+        </div>
+      </div>
+    </div>
+  </section>
 
+  <?php endwhile; ?>
+<?php endif; ?>
 
-
-
+<?php get_footer(); ?>
