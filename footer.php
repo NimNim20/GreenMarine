7c8d4113c87573bafb5acc05fb150b3ae5d24154
idaballegaard <idabal01@easv365.dk>
2024-11-19 13:36:15 +0100

    footer

--- footer.php
+++ footer.php
@@ -2,33 +2,30 @@
 <?php wp_footer(); ?>
 
 <div class="container-fluid">
-        <div class="row">
-            <div class="section footer">
-                <div class="col-3">
-                    <h3>Information</h3>
-                    <p>CVR: 593826</p>
-                </div>
-                <div class="col-3">
-                    <h3>Adresse</h3>
-                    <p>Rossagerdi 3</p>
-                    <p>530 Fuglafjørdur</p>
-                    <p>Færøerne</p>
-                </div>
-                <div class="col-3">
-                    <h3>Kontakt</h3>
-                    <p>Kontaktformular</p>
-                    <p>Tlf: +298 770060</p>
-                    <p>Mail: hk@gmd.fo</p>
-                </div>
-                <div class="col-3">
-                    <h3>Find os på</h3>
-                    <p>LinkedIn</p>
-                    <p>Facebook</p>
-                    <p>Instagram</p>
-                </div>
+    <div class="row">
+        <div class="section footer">
+            <div class="col-12">
+                <h3>Information</h3>
+                <p>CVR: 593826</p>
+        
+                <h3>Adresse</h3>
+                <p>Rossagerdi 3</p>
+                <p>530 Fuglafjørdur</p>
+                <p>Færøerne</p>
+            
+                <h3>Kontakt</h3>
+                <p>Kontaktformular</p>
+                <p>Tlf: +298 770060</p>
+                <p>Mail: hk@gmd.fo</p>
+            
+                <h3>Find os på</h3>
+                <p>LinkedIn</p>
+                <p>Facebook</p>
+                <p>Instagram</p>
             </div>
         </div>
     </div>
+</div>
 
 </body>
 </html>
