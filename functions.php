66da2976e187709382b656b3d3053f7ac836c424
NimNim20 <benvio01@easv365.dk>
2024-11-19 10:51:40 +0100

    Maintenance error

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function gmd_enqueue_styles() {
-  wp_enqueue_style("gmd-style", get_template_directory_uri() . "/style.css", array(), "1.0", "all");
-}
-
-add_action("wp_enqueue_scripts", "gmd_enqueue_styles");
