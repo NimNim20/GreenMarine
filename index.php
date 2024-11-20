11ccbe8c3b64d46d207d83d1a587c770489b5d5a
Simon Nyblom <simnyb01@easv365.dk>
2024-11-20 09:53:35 +0100

    renaming

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
 </section>
 
   <section id="home_ydelser_icons">
-    <?php get_template_part("index_template-parts/home_ydelser_icons"); ?>
+    <?php get_template_part("template-parts/home_ydelser_icons"); ?>
   </section>
 
   <section id="home_ydelser_icons">
-    <?php get_template_part("index_template-parts/home_ydelser"); ?>
+    <?php get_template_part("template-parts/home_ydelser"); ?>
   </section>
 
 
