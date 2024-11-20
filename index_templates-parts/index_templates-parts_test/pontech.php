07baabfb027c4dbf90a6781e470bf5db9f1ce245
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-11-20 09:57:17 +0100

    deleet index template

--- index_templates-parts/index_templates-parts_test/pontech.php
+++
@@ -1,4 +0,0 @@
-<section id="pontech-section">
-  <h2>Pontech Section</h2>
-  <p>Details about Pontech...</p>
-</section>
--- index_templates-parts/index_templates-parts_test/trelleborg.php
+++
@@ -1,5 +0,0 @@
-
-<section id="trelleborg-section">
-  <h2>Trelleborg Section</h2>
-  <p>Details about Trelleborg...</p>
-</section>
