300e0d060bb5eeb578684d6090204541c2e686a2
idaballegaard <idabal01@easv365.dk>
2024-11-20 14:16:02 +0100

    MB_David

--- page-mb-david.php
+++ page-mb-david.php
@@ -20,15 +20,15 @@
         <div class="row">
 
           <div class="col-12 col-md-6">
-            <div class="MB_David_img">
-              <img src="<?php echo esc_url($davidImg['url']); ?>" alt="<?php echo esc_attr($davidImg['alt']); ?>"> 
+            <div class="MB_David_text">
+              <h1><?php echo esc_html($Heading); ?></h1>
+              <p><?php echo esc_html($Description); ?></p>
             </div>
           </div>
 
           <div class="col-12 col-md-6">
-            <div class="MB_David_text">
-              <h1><?php echo esc_html($Heading); ?></h1>
-              <p><?php echo esc_html($Description); ?></p>
+            <div class="MB_David_img">
+              <img src="<?php echo esc_url($davidImg['url']); ?>" alt="<?php echo esc_attr($davidImg['alt']); ?>"> 
             </div>
           </div>
 
