e35d78d6a51647381d69e08920815477199fbb61
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-11-20 10:10:51 +0100

    home_ydelser

--- template-parts/home_ydelser.php
+++ template-parts/home_ydelser.php
@@ -4,7 +4,7 @@ $loop = new WP_Query(array(
     'posts_per_page'    => -1,
 ));?>
 
-<div class="">
+<div class="home_ydelser_boby">
 
 
     <?php if($loop->have_posts()): ?>
@@ -17,11 +17,11 @@ $loop = new WP_Query(array(
             ?>
 
 
-            <div class="icon-item">
+            <div class="home_ydelser">
                 <img src="<?php echo $image["url"] ?>" alt="" class="home_ydelser-image">
-                <h2 class="icon-title"><?php echo $title; ?></h2>
-                <p class="icon-title"><?php echo $title; ?></p>
-             </div>
+                <h2 class="home_ydelser-title"><?php echo $title; ?></h2>
+                <p class="home_ydelser_text"><?php echo $text; ?></p>
+            </div>
            
           
 
