3bec0cb22a1d6b4a035bcfe3f3f1fbe67abd35ab
Simon Nyblom <simnyb01@easv365.dk>
2024-11-20 14:30:37 +0100

    Trying to get id linking to scroll functionality

--- template-parts/home_ydelser.php
+++ template-parts/home_ydelser.php
@@ -6,28 +6,28 @@ $loop = new WP_Query(array(
 
 <div class="home_ydelser_boby">
 
+<?php if($loop->have_posts()): ?>
+    <?php while($loop->have_posts()): $loop->the_post() ?>
 
-    <?php if($loop->have_posts()): ?>
-        <?php while($loop->have_posts()): $loop->the_post() ?>
+        <?php
+        $title = get_the_title();
+        $text = get_field("text");
+        $image = get_field("image");
+        $post_id = get_the_ID(); // Unique ID for each post
+        ?>
 
-            <?php
-            $title = get_the_title();
-            $text = get_field("text");
-            $image = get_field("image");
-            ?>
-
-
-            <div class="home_ydelser">
-                <div class="img_box_home_ydelser"><img src="<?php echo $image["url"] ?>" alt="" class="home_ydelser-image"></div>
-                <div class="home_ydelser_title_text">
-                    <h2 class="home_ydelser-title"><?php echo $title; ?></h2>
-                    <p class="home_ydelser_text"><?php echo $text; ?></p>
-                </div>
+        <div id="section-<?php echo $post_id; ?>" class="home_ydelser">
+            <div class="img_box_home_ydelser">
+                <img src="<?php echo $image["url"] ?>" alt="" class="home_ydelser-image">
             </div>
-           
-          
+            <div class="home_ydelser_title_text">
+                <h2 class="home_ydelser-title"><?php echo $title; ?></h2>
+                <p class="home_ydelser_text"><?php echo $text; ?></p>
+            </div>
+        </div>
 
-        <?php endwhile; ?>
-    <?php endif; ?>
+    <?php endwhile; ?>
+<?php endif; ?>
+
   
 </div>
