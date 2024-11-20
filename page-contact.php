1cd5f12913e072ed1ac6594433443679bc68af29
Simon Nyblom <simnyb01@easv365.dk>
2024-11-20 12:31:37 +0100

    Added a template part and put it on everypage

--- page-contact.php
+++ page-contact.php
@@ -2,7 +2,9 @@
 
 <?php if(have_posts()) : ?>
   <?php while(have_posts()) : the_post(); ?>
-
+  <section id="navButtons">
+    <?php get_template_part("template-parts/navbuttons"); ?>
+  </section>
   <section class="contactForm">
     <?php echo do_shortcode ('[cf7form cf7key="greenmarinecontact"]'); ?>
   </section>
