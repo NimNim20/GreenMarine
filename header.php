929bfd65937d561857531dcd4344ce7025d57a56
Simon Nyblom <simnyb01@easv365.dk>
2024-11-21 15:35:59 +0100

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Green Marine Divers</title>
     <section id="mobileHero"
     <?php 
     $hero_image = get_field('heroimage');
