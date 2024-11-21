1ad987c1a04996f6a9ea5383e86d4de6e8150c76
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-11-21 20:15:50 +0100

    fron page test

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post(); ?>
-  <section id="navButtons">
-    <?php get_template_part("template-parts/navbuttons"); ?>
-  </section>
-
-  <section id="home_ydelser_icons">
-    <?php get_template_part("template-parts/home_ydelser_icons"); ?>
-  </section>
-
-  <section id="home_ydelser">
-    <?php get_template_part("template-parts/home_ydelser"); ?>
-  </section>
-
-
-
-  <?php endwhile; ?>
-<?php endif; ?>
-
-<?php get_footer(); ?>
+<p>Test</p>
