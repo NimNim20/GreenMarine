11f56332e36c1f49c7ef74ae2eb2c8cd544b88ce
Simon Nyblom <simnyb01@easv365.dk>
2024-11-21 09:47:31 +0100

    reverting

--- header.php
+++ header.php
@@ -7,22 +7,26 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<section id="mobileHero" style="background-image: url('<?php echo esc_url(get_hero_image_url()); ?>');">
-    <div class="hero-header">
+    <section id="mobileHero">
+<div class="hero-header">
         <div class="navLogos">
-            <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/logo1.png" alt="Logo 1" class="logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/logo2.png" alt="Logo 2" class="logo"> -->
+            <img alt="Logo 1" class="logo">
+                <img alt="Logo 2" class="logo">
         </div>
-        <div class="nav">
-            <a href="<?php echo site_url('/contact')?>" class="kontaktLink">Kontakt</a>
-            <div class="aboutUs">Om os</div>
-        </div>
-    </div>
-    <div class="hero-footer">
-        <a href="<?php echo site_url('/contact')?>">
-            <button class="kontaktButton">KONTAKT</button>
+    <div class="nav">
+        <a href="<?php echo site_url('/contact')?>" class="kontaktLink">
+            Kontakt
         </a>
+        <div class="aboutUs">
+            Om os
+        </div>
     </div>
-</section>
+</div>
+        <div class="hero-footer">
+            <a href="<?php echo site_url('/contact')?>">
+                <button class="kontaktButton">KONTAKT</button>
+            </a>
+        </div>
+    </section>
 </body>
 </html>
