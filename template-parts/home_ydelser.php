9272936f34b8420badc4d7d0bf47f9eb102bf6f1
Simon Nyblom <simnyb01@easv365.dk>
2024-11-21 13:04:24 +0100

    Update home_ydelser.php

--- template-parts/home_ydelser.php
+++ template-parts/home_ydelser.php
@@ -17,13 +17,13 @@ $loop = new WP_Query(array(
                 $post_id = get_the_ID(); // Unique ID for each post
                 ?>
 
-                <div class="row home_ydelser">
+                <div class="row home_ydelser" data-title="<?php echo esc_attr($title); ?>">
                     <div class="col-12 col-sm-6 img_box_home_ydelser">
                         <img src="<?php echo $image['url']; ?>" alt="<?php echo esc_attr($title); ?>" class="home_ydelser-image" loading="lazy">
                     </div>
                     <div class="col-12 col-sm-6 home_ydelser_title_text">
                         <div class="home_ydelser_title_text_inder_box">
-                            <h2 class="home_ydelser-title"  data-title="<?php echo $title; ?>"><?php echo $title; ?></h2>
+                            <h2 class="home_ydelser-title"><?php echo $title; ?></h2>
                             <p class="home_ydelser_text"><?php echo $text; ?></p>
                         </div>
                     </div>
