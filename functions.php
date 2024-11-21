ec84574600f17dacea2ee6540a5dde7e6b015907
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-11-21 20:23:44 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function gmd_load_resources() {
     wp_enqueue_script("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", array(), null, true);
 
     // The following returns by default style.css in the current active theme directory.
-    wp_enqueue_style("style", get_template_directory_uri() . "/style.css", array(), filemtime(get_template_directory() . "/style.css"), true);
+    wp_enqueue_style("my-theme-style", get_stylesheet_uri());
     wp_enqueue_script("script", get_template_directory_uri() . "/script.js", array(), filemtime(get_template_directory() . "/script.js"), true);
 
     // Enqueue anime.js
@@ -19,4 +19,3 @@ function gmd_disable_gutenberg() {
 }
 add_action("init", "gmd_disable_gutenberg");
 
-?>
