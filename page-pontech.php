21a3a060d65ccf398ea7911c46993dac3e933768
idaballegaard <idabal01@easv365.dk>
2024-11-21 13:55:27 +0100

    pontech classes

--- page-pontech.php
+++ page-pontech.php
@@ -18,9 +18,9 @@
     <section>
       <div class="container-fluid p-0">
         <div class="row">
-          <div class="MB_David">
+          <div class="page-content">
             <div class="col-12 col-md-6">
-              <div class="MB_David_text pontech-text">
+              <div class="content-text pontech-text">
                 <h1><?php echo esc_html($Heading); ?></h1>
                 <p><?php echo esc_html($Description); ?></p>
                 <div class="trelleborg-btn-container">
@@ -31,7 +31,7 @@
             </div>
 
             <div class="col-12 col-md-6">
-              <div class="MB_David_img">
+              <div class="content_img">
                 <img src="<?php echo esc_url($pontechImg['url']); ?>" alt="<?php echo esc_attr($pontechImg['alt']); ?>" loading="lazy"> 
               </div>
             </div>
