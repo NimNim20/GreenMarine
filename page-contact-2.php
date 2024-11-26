23a5c9a5202080e6dd5edeac221fcc61c78f25b6
NimNim20 <benvio01@easv365.dk>
2024-11-26 13:23:50 +0100

    Contact template part

---
+++ page-contact-2.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<?php if(have_posts()) : ?>
+  <?php while(have_posts()) : the_post(); ?>
+
+  <?php
+    $formtext = get_field('formtext');
+  ?>
+
+  <section id="navButtons">
+    <?php get_template_part("template-parts/navbuttons"); ?>
+  </section>
+
+  <section>
+    <?php get_template_part("template-parts/contact"); ?>
+  </section>
+
+  <?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
