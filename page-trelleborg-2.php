ef8f9d26fe81c0ec12fdd5279d556b357f81af85
NimNim20 <benvio01@easv365.dk>
2024-11-26 13:21:11 +0100

    Trelleborg template part

---
+++ page-trelleborg-2.php
@@ -0,0 +1,20 @@
+<?php get_header (); ?>
+
+
+
+
+<?php if(have_posts()) : ?>
+    <?php while(have_posts()) : the_post(); ?>
+
+    <section id="navButtons">
+      <?php get_template_part("template-parts/navbuttons"); ?>
+    </section>
+
+    <section>
+      <?php get_template_part("template-parts/trelleborg"); ?>
+    </section>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
