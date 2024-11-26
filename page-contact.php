e139914e8d0862a567246e94dde8a11a189f8001
NimNim20 <benvio01@easv365.dk>
2024-11-26 09:27:17 +0100

    Styling of forms

--- page-contact.php
+++ page-contact.php
@@ -12,7 +12,7 @@
   </section>
 
   <section class="contactForm">
-    <h1><?php echo $formtext; ?></h1>
+    <h1 class="ContactHeader"><?php echo $formtext; ?></h1>
     <?php echo do_shortcode ('[contact-form-7 id="75513d2" title="GreenMarineContact"]'); ?>
   </section>
 
