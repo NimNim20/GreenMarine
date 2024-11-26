4e3639dab148eeb0ac65572f607a067b7f2e7926
NimNim20 <benvio01@easv365.dk>
2024-11-26 09:26:20 +0100

    Added dynamic header title for contact page

--- page-contact.php
+++ page-contact.php
@@ -2,10 +2,17 @@
 
 <?php if(have_posts()) : ?>
   <?php while(have_posts()) : the_post(); ?>
+
+  <?php
+    $formtext = get_field('formtext');
+  ?>
+
   <section id="navButtons">
     <?php get_template_part("template-parts/navbuttons"); ?>
   </section>
+
   <section class="contactForm">
+    <h1><?php echo $formtext; ?></h1>
     <?php echo do_shortcode ('[contact-form-7 id="75513d2" title="GreenMarineContact"]'); ?>
   </section>
 
