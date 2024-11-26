7e8b2d5ad5523bcaa3b13b41bb2cc4c0cc634c47
idaballegaard <idabal01@easv365.dk>
2024-11-26 14:26:29 +0100

    Moved styling til another css file

--- page-om-os.php
+++ page-om-os.php
@@ -38,7 +38,7 @@
             <div class="col-12 col-md-6">
               <div class="content_img about-images">
                 <img src="<?php echo esc_url($aboutImg['url']); ?>" alt="<?php echo esc_attr($aboutImg['alt']); ?>" loading="lazy">
-                
+                <img src="<?php echo esc_url($aboutImg2['url']); ?>" alt="<?php echo esc_attr($aboutImg2['alt']); ?>" loading="lazy">  
               </div>
             </div>
           </div>
