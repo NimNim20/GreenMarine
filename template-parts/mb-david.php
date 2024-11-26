daae8e28ae30900be9602db9f791c4361fc9e6d4
NimNim20 <benvio01@easv365.dk>
2024-11-26 13:09:09 +0100

    Mb David template part

---
+++ template-parts/mb-david.php
@@ -0,0 +1,70 @@
+<?php if(have_posts()) : ?>
+    <?php while(have_posts()) : the_post(); ?>
+    <section id="navButtons">
+      <?php get_template_part("template-parts/navbuttons"); ?>
+    </section>
+
+    <?php
+    $Heading = get_field("heading");
+    $Description = get_field("description");
+    $servicesHeading = get_field("services-heading");
+    $serviceDiving = get_field("service-diving");
+    $divingDescription = get_field("diving-description");
+    $fishfarmHeading = get_field("fishfarm-heading");
+    $fishfarmDescription = get_field("fishfarm-description");
+    $davidImg2 = get_field("david-img2");
+    $davidImg = get_field("david-img");
+    $contactText = get_field("contact-text");
+    ?>
+
+    <section>
+      <div class="container-fluid p-0">
+        <div class="row">
+          <div class="page-content">
+            <div class="col-12 col-md-6">
+              <div class="content-text">
+                <h1><?php echo esc_html($Heading); ?></h1>
+                <p><?php echo esc_html($Description); ?></p>
+              </div>
+            </div>
+
+            <div class="col-12 col-md-6">
+              <div class="content_img">
+                <img src="<?php echo esc_url($davidImg['url']); ?>" alt="<?php echo esc_attr($davidImg['alt']); ?>" loading="lazy"> 
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="page-content2">
+            <div class="col-12 col-md-6">
+              <div class="content-text text2">
+                <h2><?php echo esc_html($servicesHeading); ?></h2>
+                <h3><?php echo esc_html($serviceDiving); ?></h3>
+                <p><?php echo esc_html($divingDescription); ?></p>
+                <h3><?php echo esc_html($fishfarmHeading); ?></h3>
+                <p><?php echo esc_html($fishfarmDescription); ?></p>
+                <h3><?php echo esc_html($contactText); ?></h3>
+
+                <div class="trelleborg-btn-container">
+                  <a href="<?php echo site_url('/contact')?>" class="trelleborg-btn">Kontakt</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-12 col-md-6">
+              <div class="content_img img2">
+                <img src="<?php echo esc_url($davidImg2['url']); ?>" alt="<?php echo esc_attr($davidImg2['alt']); ?>" loading="lazy"> 
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <?php endwhile; ?>
+<?php endif; ?>
+
