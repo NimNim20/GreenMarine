286532184a4eb0513b066e00f5aabfd083101c4f
NimNim20 <benvio01@easv365.dk>
2024-11-26 13:19:16 +0100

    Pontech template part

---
+++ template-parts/pontech.php
@@ -0,0 +1,30 @@
+<?php
+    $Heading = get_field("heading");
+    $Description = get_field("description");
+    $pontechImg = get_field("pontech_img");
+    ?>
+
+    <section>
+      <div class="container-fluid p-0">
+        <div class="row">
+          <div class="page-content">
+            <div class="col-12 col-md-6">
+              <div class="content-text pontech-text">
+                <h1><?php echo esc_html($Heading); ?></h1>
+                <p><?php echo esc_html($Description); ?></p>
+                <div class="trelleborg-btn-container">
+                <a href="https://pontechmarina.com/se/" class="trelleborg-btn" target="_blank">Pontech's side</a>
+                </div>
+                
+              </div>
+            </div>
+
+            <div class="col-12 col-md-6">
+              <div class="content_img">
+                <img src="<?php echo esc_url($pontechImg['url']); ?>" alt="<?php echo esc_attr($pontechImg['alt']); ?>" loading="lazy"> 
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
