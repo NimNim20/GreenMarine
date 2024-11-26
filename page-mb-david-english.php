88becac919e1e88cd1bfa2d59e0c4c08d7d75dc4
NimNim20 <benvio01@easv365.dk>
2024-11-26 13:12:33 +0100

    Mb David template part

--- page-mb-david-english.php
+++
@@ -1,9 +0,0 @@
-<?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post(); ?>
-
-  <section>
-    <?php get_template_part("template-parts/mb-david"); ?>
-  </section>
-
-  <?php endwhile; ?>
-<?php endif; ?>
