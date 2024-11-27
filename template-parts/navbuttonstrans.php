ca486f84b53d234e6f12159dc4b46568d510c555
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 09:30:50 +0100

    reverted

--- template-parts/navbuttonstrans.php
+++ template-parts/navbuttonstrans.php
@@ -1,8 +1,8 @@
 <section class="content">
   <div class="button-grid">
-    <a href="<?php echo site_url('/green-marine/')?>" class="btn green-marine">Green Marine</a>
-    <a href="<?php echo site_url('/mb-david-english/')?>" class="btn mb-david">MB David</a>
-    <a href="<?php echo site_url('/pontech-2/')?>" class="btn pontech">Pontech</a>
-    <a href="<?php echo site_url('/trelleborg-2/')?>" class="btn trelleborg">Trelleborg</a>
+    <a href="<?php echo site_url('/en/green-marine/')?>" class="btn green-marine">Green Marine</a>
+    <a href="<?php echo site_url('/en/mb-david-english/')?>" class="btn mb-david">MB David</a>
+    <a href="<?php echo site_url('/en/pontech-2/')?>" class="btn pontech">Pontech</a>
+    <a href="<?php echo site_url('/en/trelleborg-2/')?>" class="btn trelleborg">Trelleborg</a>
   </div>
 </section>
