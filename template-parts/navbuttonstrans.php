92dde639ab85e690b04c423f551afa0b32036d2f
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 13:58:04 +0100

    trying matching slugs to translations

--- template-parts/navbuttonstrans.php
+++ template-parts/navbuttonstrans.php
@@ -1,8 +1,8 @@
 <section class="content">
   <div class="button-grid">
-    <a href="<?php echo site_url('/en/green-marine/')?>" class="btn green-marine">Green Marine</a>
-    <a href="<?php echo site_url('/en/mb-david-english/')?>" class="btn mb-david">MB David</a>
-    <a href="<?php echo site_url('/en/pontech-2/')?>" class="btn pontech">Pontech</a>
-    <a href="<?php echo site_url('/en/trelleborg-2/')?>" class="btn trelleborg">Trelleborg</a>
+    <a href="<?php echo pll_home_url(); ?>green-marine/" class="btn green-marine"><?php pll_e("Green Marine"); ?></a>
+    <a href="<?php echo pll_home_url(); ?>mb-david/" class="btn mb-david"><?php pll_e("MB David"); ?></a>
+    <a href="<?php echo pll_home_url(); ?>pontech/" class="btn pontech"><?php pll_e("Pontech"); ?></a>
+    <a href="<?php echo pll_home_url(); ?>trelleborg/" class="btn trelleborg"><?php pll_e("Trelleborg"); ?></a>
   </div>
 </section>
