96a99e96530652824bba9e0842f0b3b8c6c3c177
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 21:32:45 +0100

    trying to load 3 images at once

--- template-parts/customcarousel.php
+++ template-parts/customcarousel.php
@@ -1,31 +1,42 @@
 <div id="customCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <?php
-        // Fetch all carousel posts
         $loop = new WP_Query(array(
             "post_type" => "carousel",
             "posts_per_page" => -1
         ));
 
         $active_class_set = false;
+        $counter = 0; 
+        
         if ($loop->have_posts()):
             while ($loop->have_posts()): $loop->the_post();
 
                 $carouselimage = get_field('carouselimage');
                 $alt_text = get_field('alt_text');
-                // Check if the image exists
+                
                 if ($carouselimage):
+                    if ($counter % 3 == 0): 
+                        if ($counter > 0): 
+                            echo '</div>'; 
+                        endif;
+                        
+                        echo '<div class="carousel-item ' . (!$active_class_set ? 'active' : '') . '">';
+                        echo '<div class="row">'; 
+                    endif;
                     ?>
-                    <div class="carousel-item <?php echo !$active_class_set ? 'active' : ''; ?>">
-                        <div class="row">
-                            <div class="col-12 col-md-4">
-                                <img src="<?php echo esc_url($carouselimage['url']); ?>" 
-                                     class="d-block w-100" 
-                                     alt="<?php echo esc_attr($alt_text); ?>">
-                            </div>
-                        </div>
+                    <div class="col-12 col-md-4">
+                        <img src="<?php echo esc_url($carouselimage['url']); ?>" 
+                             class="d-block w-100" 
+                             alt="<?php echo esc_attr($alt_text); ?>">
                     </div>
                     <?php
+                    $counter++;
+                    if ($counter % 3 == 0 || $counter == $loop->post_count): 
+                        // If 3 images have been added or it's the last image, close the row and the slide
+                        echo '</div>'; 
+                        echo '</div>';
+                    endif;
                     $active_class_set = true; 
                 endif;
 
