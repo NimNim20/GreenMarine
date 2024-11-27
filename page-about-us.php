3b6c4c9952081ca595949db01aae792b823bcbb8
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 14:15:24 +0100

    fixed about us page

---
+++ page-about-us.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<?php if(have_posts()) : ?>
+  <?php while(have_posts()) : the_post(); ?>
+  <section id="navButtons">
+    <?php get_template_part("template-parts/navbuttons"); ?>
+  </section>
+
+  <section>
+    <?php get_template_part("template-parts/aboutus"); ?>
+  </section>
+
+  <?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
