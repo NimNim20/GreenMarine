895558c6aa1c4b1eac56829664f7248d18a85fbf
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 09:47:50 +0100

    finished the slug

--- template-parts/navbuttonstrans.php
+++ template-parts/navbuttonstrans.php
@@ -1,6 +1,6 @@
 <section class="content">
   <div class="button-grid">
-    <a href="<?php echo site_url('/en/green-marine')?>" class="btn green-marine">Green Marine</a>
+    <a href="<?php echo site_url('/en/green-marine/')?>" class="btn green-marine">Green Marine</a>
     <a href="<?php echo site_url('/en/mb-david-english/')?>" class="btn mb-david">MB David</a>
     <a href="<?php echo site_url('/en/pontech-2/')?>" class="btn pontech">Pontech</a>
     <a href="<?php echo site_url('/en/trelleborg-2/')?>" class="btn trelleborg">Trelleborg</a>
