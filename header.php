e3b8c9e81d52184c652258d9f83d9e0eb8b98b12
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 12:37:31 +0100

    attempt to fix polylang flags

--- header.php
+++ header.php
@@ -8,13 +8,16 @@
 </head>
 <body <?php body_class(); ?>>
     <header>
-        <div class="lang"> 
-            <?php 
-                echo pll_the_languages(array(
-                'show_flags' => -1,
-                'show_names' => -1
-            )); 
-            ?>
-        </div>
+    <div class="lang">
+    <?php 
+        echo pll_the_languages(array(
+            'show_flags' => 1,       // Show only flags
+            'show_names' => 0,       // Hide language names
+            'hide_if_empty' => 1,    // Hide if no translations are available
+            'echo' => 0              // Return as string to avoid unexpected behavior
+        )); 
+    ?>
+    </div>
+
         <?php get_template_part("template-parts/header_section"); ?>
     </header>
