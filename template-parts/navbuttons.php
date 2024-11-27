a2eba960d7da7864a12a70e4dda6658b0266a27e
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 13:56:56 +0100

    Update navbuttons.php

--- template-parts/navbuttons.php
+++ template-parts/navbuttons.php
@@ -1,8 +1,8 @@
 <section class="content">
   <div class="button-grid">
-    <a href="<?php echo site_url('/')?>" class="btn green-marine">Green Marine</a>
-    <a href="<?php echo site_url('/mb-david/')?>" class="btn mb-david">MB David</a>
-    <a href="<?php echo site_url('/pontech/')?>" class="btn pontech">Pontech</a>
-    <a href="<?php echo site_url('/trelleborg/')?>" class="btn trelleborg">Trelleborg</a>
+    <a href="<?php echo pll_home_url(); ?>green-marine/" class="btn green-marine"><?php pll_e("Green Marine"); ?></a>
+    <a href="<?php echo pll_home_url(); ?>mb-david/" class="btn mb-david"><?php pll_e("MB David"); ?></a>
+    <a href="<?php echo pll_home_url(); ?>pontech/" class="btn pontech"><?php pll_e("Pontech"); ?></a>
+    <a href="<?php echo pll_home_url(); ?>trelleborg/" class="btn trelleborg"><?php pll_e("Trelleborg"); ?></a>
   </div>
 </section>
