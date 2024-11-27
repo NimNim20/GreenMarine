1a22688454e8e919ba52f9e019d6fbe1bce3c414
NimNim20 <benvio01@easv365.dk>
2024-11-27 13:24:31 +0100

    Translation

---
+++ page-kontakt.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<?php if(have_posts()) : ?>
+  <?php while(have_posts()) : the_post(); ?>
+
+  <?php
+    $formtext = get_field('formtext');
+  ?>
+
+  <section id="navButtons">
+    <?php get_template_part("template-parts/navbuttons"); ?>
+  </section>
+
+  <section class="contactForm">
+    <h1 class="ContactHeader"><?php echo $formtext; ?></h1>
+    <?php echo do_shortcode ('[contact-form-7 id="75513d2" title="GreenMarineContact"]'); ?>
+  </section>
+
+  <section id="scrolltotopbutton">
+    <?php get_template_part ("template-parts/scrolltotop"); ?>
+  </section>
+
+  <?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
