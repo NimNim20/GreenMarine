e02150e1113ca84a09a90cddf336bbc93dc9f484
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 12:43:56 +0100

    removed names from translations

--- template-parts/header_section.php
+++ template-parts/header_section.php
@@ -30,8 +30,7 @@
     <div class="lang"> 
             <?php 
                 pll_the_languages(array(
-                'show_flags' => -1,
-                'show_names' => -1
+                'show_flags' => -1
             )); 
             ?>
         </div>
