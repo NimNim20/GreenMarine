a1b1787da012aca68bbd9c937d42faf3d25955c6
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 09:15:59 +0100

    translations C:

--- header.php
+++ header.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Green Marine Divers</title>
-    <?php wp_head() ?> 
+    <?php wp_head(); ?> 
 </head>
+<body <?php body_class(); ?>>
+    <header>
         <div class="lang"> 
-            <?php pll_the_languages() ?>
-        </div> 
-<body <?php body_class() ?>>
-
-<?php get_template_part("template-parts/header_section"); ?>
+            <?php 
+            echo pll_the_languages(array(
+                'show_flags' => 1,
+                'show_names' => 1
+            )); 
+            ?>
+        </div>
+        <?php get_template_part("template-parts/header_section"); ?>
+    </header>
