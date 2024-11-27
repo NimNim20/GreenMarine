897f84f42724c71181ff44950ac683de686ca3dc
Simon Nyblom <simnyb01@easv365.dk>
2024-11-27 10:10:20 +0100

    bootstrap carousel

---
+++ template-parts/customcarousel.php
@@ -0,0 +1,41 @@
+<div id="customCarousel" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <!-- Slide 1 -->
+    <div class="carousel-item active">
+      <div class="row">
+        <div class="col-12 col-md-4">
+          <img src="image1.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="col-12 col-md-4">
+          <img src="image2.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="col-12 col-md-4">
+          <img src="image3.jpg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+    </div>
+    <!-- Slide 2 -->
+    <div class="carousel-item">
+      <div class="row">
+        <div class="col-12 col-md-4">
+          <img src="image4.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="col-12 col-md-4">
+          <img src="image5.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="col-12 col-md-4">
+          <img src="image6.jpg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+    </div>
+    <!-- Add more slides as needed -->
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#customCarousel" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#customCarousel" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
