335c4d0b52cf214795b90a43fa6aab83322ea87a
Simon Nyblom <simnyb01@easv365.dk>
2024-11-28 09:38:01 +0100

    trying a new carousel

--- template-parts/customcarousel.php
+++ template-parts/customcarousel.php
@@ -1,54 +1,61 @@
-<div id="customCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <?php
-        // Fetch all carousel posts
-        $loop = new WP_Query(array(
-            "post_type" => "carousel",
-            "posts_per_page" => -1
-        ));
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+              
+              <!-- Carousel Indicators -->
+              <ol class="carousel-indicators">
+                <?php
+                $loop = new WP_Query(array(
+                  "post_type" => "carousel", // Adjust post type if needed
+                  "posts_per_page" => -1
+                ));
+                $index = 0;
+                ?>
 
-        $active_class_set = false;
-        $counter = 0;
-
-        if ($loop->have_posts()):
-            while ($loop->have_posts()): $loop->the_post();
-
-                $carouselimage = get_field('carouselimage');
-                $alt_text = get_field('alt_text');
-
-                if ($carouselimage):
-                    // Start a new carousel-item every 3 images
-                    if ($counter % 3 == 0):
-                        if ($counter > 0): 
-                            echo '</div>'; // Close previous row
-                            echo '</div>'; // Close previous carousel-item
-                        endif;
-                        echo '<div class="carousel-item ' . (!$active_class_set ? 'active' : '') . '">';
-                        echo '<div class="row">'; // Open a new row
-                    endif;
-                    ?>
-                    <div class="col-12 col-md-4">
-                        <img src="<?php echo esc_url($carouselimage['url']); ?>" 
-                             class="d-block w-100" 
-                             alt="<?php echo esc_attr($alt_text); ?>">
-                    </div>
+                <?php if($loop->have_posts()): ?>
+                  <?php while($loop->have_posts()): $loop->the_post(); ?>
+                    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?php echo $index; ?>" class="<?php echo ($index == 0) ? "active" : "" ?>"></li>
+                    <?php $index++; ?>
+                  <?php endwhile; ?>
+                  <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
+              </ol>
+              
+              <!-- Carousel Inner -->
+              <div class="carousel-inner">
+                <?php $index = 0; ?>
+                <?php if($loop->have_posts()): ?>
+                  <?php while($loop->have_posts()): $loop->the_post(); ?>
                     <?php
-                    $counter++;
-                    $active_class_set = true;
-                endif;
+                    // Get SCF custom fields
+                    $carouselImage = scf('carouselimage'); // SCF custom field for the image
+                    $altText = scf('alt_text'); // SCF custom field for alt text
+                    ?>
+                    <!-- Carousel Item -->
+                    <div class="carousel-item <?php echo ($index == 0) ? "active" : "" ?>">
+                      <img class="d-block w-100" src="<?php echo esc_url($carouselImage['url']); ?>" alt="<?php echo esc_attr($altText); ?>">
+                    </div>
+                    <?php $index++; ?>
+                  <?php endwhile; ?>
+                  <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
+              </div>
 
-            endwhile;
-
-            // Close any remaining open tags
-            if ($counter % 3 != 0):
-                echo '</div>'; // Close last row
-                echo '</div>'; // Close last carousel-item
-            endif;
-
-            wp_reset_postdata();
-        endif;
-        ?>
+              <!-- Carousel Controls -->
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+        </div>
     </div>
+</div>
+
 
     <!-- Carousel controls -->
     <button class="carousel-control-prev" type="button" data-bs-target="#customCarousel" data-bs-slide="prev">
