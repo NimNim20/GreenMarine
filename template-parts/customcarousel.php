bfc8a73c8d4b95e2861ed81a2bd92e8b7a461cc0
Simon Nyblom <simnyb01@easv365.dk>
2024-11-28 09:32:41 +0100

    trying to remove bootstrap hidden

--- template-parts/customcarousel.php
+++ template-parts/customcarousel.php
@@ -57,11 +57,11 @@
 
     <!-- Carousel controls -->
     <button class="carousel-control-prev" type="button" data-bs-target="#customCarousel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="carousel-control-prev-icon"></span>
         <span class="visually-hidden">Previous</span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#customCarousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="carousel-control-next-icon"></span>
         <span class="visually-hidden">Next</span>
     </button>
 </div>
