4b394826259805be5ac84cac59aa9a9f0dc09195
Simon Nyblom <simnyb01@easv365.dk>
2024-11-29 20:11:53 +0100

    Update mb-david.php

--- template-parts/mb-david.php
+++ template-parts/mb-david.php
@@ -12,14 +12,16 @@
     ?>
 
 
-<section>
+<section id="contentDavid">
       <div class="container-fluid p-0">
         <div class="row">
           <div class="page-content">
             <div class="col-12 col-md-6">
               <div class="content-text">
+                <article id="headerContent">
                 <h1><?php echo esc_html($Heading); ?></h1>
                 <p><?php echo esc_html($Description); ?></p>
+                </article>
               </div>
             </div>
 
@@ -58,7 +60,7 @@
           </div>
         </div>
       </div>
-    </section>
+</section>
 
 
 
