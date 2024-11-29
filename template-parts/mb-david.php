2696f0e50680af235c01715a3a985d4741fcccb0
Simon Nyblom <simnyb01@easv365.dk>
2024-11-29 19:59:38 +0100

    Reverted

--- template-parts/mb-david.php
+++ template-parts/mb-david.php
@@ -11,62 +11,53 @@
     $contactText = get_field("contact-text");
     ?>
 
-<section>
-  <div class="container-fluid p-0">
-    <div class="row">
-      <article class="page-content">
-        <!-- Text Content Section -->
-        <div class="col-12 col-md-6">
-          <header class="content-text">
-            <h1><?php echo esc_html($Heading); ?></h1>
-            <p><?php echo esc_html($Description); ?></p>
-          </header>
-        </div>
 
-        <!-- Image Section -->
-        <div class="col-12 col-md-6">
-          <div class="content_img">
-            <img class="img-fluid" src="<?php echo esc_url($davidImg['url']); ?>" alt="<?php echo esc_attr($davidImg['alt']); ?>" loading="lazy"> 
+<section>
+      <div class="container-fluid p-0">
+        <div class="row">
+          <div class="page-content">
+            <div class="col-12 col-md-6">
+              <div class="content-text">
+                <h1><?php echo esc_html($Heading); ?></h1>
+                <p><?php echo esc_html($Description); ?></p>
+              </div>
+            </div>
+
+            <div class="col-12 col-md-6">
+              <div class="content_img">
+                <img class="img-fluid" src="<?php echo esc_url($davidImg['url']); ?>" alt="<?php echo esc_attr($davidImg['alt']); ?>" loading="lazy"> 
+              </div>
+            </div>
           </div>
         </div>
-      </article>
-    </div>
 
-    <div class="row">
-      <article class="page-content2">
-        <!-- Services Text Section -->
-        <div class="col-12 col-md-6">
-          <header class="content-text text2">
-            <h2><?php echo esc_html($servicesHeading); ?></h2>
+        <div class="row">
+          <div class="page-content2">
+            <div class="col-12 col-md-6">
+              <div class="content-text text2">
+                <h2><?php echo esc_html($servicesHeading); ?></h2>
+                <h3><?php echo esc_html($serviceDiving); ?></h3>
+                <p><?php echo esc_html($divingDescription); ?></p>
+                <h3><?php echo esc_html($fishfarmHeading); ?></h3>
+                <p><?php echo esc_html($fishfarmDescription); ?></p>
+                <h3><?php echo esc_html($contactText); ?></h3>
 
-          <section>
-            <h3><?php echo esc_html($serviceDiving); ?></h3>
-            <p><?php echo esc_html($divingDescription); ?></p>
-          </section>
-          <section>
-            <h3><?php echo esc_html($fishfarmHeading); ?></h3>
-            <p><?php echo esc_html($fishfarmDescription); ?></p>
-          </section>
-          <section>
-            <h3><?php echo esc_html($contactText); ?></h3>
-            </header>
-            <div class="trelleborg-btn-container">
-              <a href="<?php echo site_url('/contact')?>" class="trelleborg-btn"><?php pll_e("Kontakt") ?></a>
-            </div>
-                    <!-- Image Section -->
-              <div class="col-12 col-md-6">
-                <div class="content_img img2">
-                  <img class="img-fluid" src="<?php echo esc_url($davidImg2['url']); ?>" alt="<?php echo esc_attr($davidImg2['alt']); ?>" loading="lazy"> 
+                <div class="trelleborg-btn-container">
+                  <a href="<?php echo site_url('/contact')?>" class="trelleborg-btn"><?php pll_e("Kontakt") ?></a>
                 </div>
               </div>
-          </section>
+            </div>
+
+            <div class="col-12 col-md-6">
+              <div class="content_img img2">
+                <img class="img-fluid" src="<?php echo esc_url($davidImg2['url']); ?>" alt="<?php echo esc_attr($davidImg2['alt']); ?>" loading="lazy"> 
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+    </section>
 
-
-      </article>
-    </div>
-  </div>
-</section>
 
 
     <section id="scrolltotopbutton">
