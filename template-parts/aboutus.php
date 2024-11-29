353f972805022a0ca4b5d581d7366f893e46b224
idaballegaard <idabal01@easv365.dk>
2024-11-29 10:26:13 +0100

    Update aboutus.php

--- template-parts/aboutus.php
+++ template-parts/aboutus.php
@@ -17,7 +17,7 @@
     <section>
       <div class="container-fluid p-0">
         <div class="row">
-          <div class="page-content2">
+          <div class="about-content">
             <div class="col-12 col-md-6">
               <div class="about-text">
                 <h1 class="about-heading"><?php echo esc_html($Heading); ?></h1>
