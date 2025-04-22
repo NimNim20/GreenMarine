099f68289383a4235325ab089fc13678511b9825
Benjamin Vid Gard <benvio01@easv365.dk>
2025-04-22 09:15:41 +0200

    heeeeeelp

--- functions.php
+++ functions.php
@@ -1,17 +1,17 @@
 <?php
 
 function gmd_load_resources() {
-    wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
-    wp_enqueue_script("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", array(), null, true);
-    wp_enqueue_style('bootstrap-icons', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css', [], '1.10.5');
+    // wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
+    // wp_enqueue_script("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", array(), null, true);
+    // wp_enqueue_style('bootstrap-icons', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css', [], '1.10.5');
 
-    // The following returns by default style.css in the current active theme directory.
-    wp_enqueue_style("my-theme-style", get_stylesheet_uri());
-    wp_enqueue_style('my-theme-home-style', get_theme_file_uri('style-home.css') );
-    wp_enqueue_script("script", get_template_directory_uri() . "/script.js", array(), filemtime(get_template_directory() . "/script.js"), true);
+    // // The following returns by default style.css in the current active theme directory.
+    // wp_enqueue_style("my-theme-style", get_stylesheet_uri());
+    // wp_enqueue_style('my-theme-home-style', get_theme_file_uri('style-home.css') );
+    // wp_enqueue_script("script", get_template_directory_uri() . "/script.js", array(), filemtime(get_template_directory() . "/script.js"), true);
 
-    // Enqueue anime.js
-    wp_enqueue_script("anime-js", "https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js", array(), null, true);
+    // // Enqueue anime.js
+    // wp_enqueue_script("anime-js", "https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js", array(), null, true);
 }
 add_action("wp_enqueue_scripts", "gmd_load_resources");
 
